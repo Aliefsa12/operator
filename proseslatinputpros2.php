19ac8b143ad8c83f5d19240982bacdbbfc87e56f
aliefsa12 <aliefsa12@gmail.com>
2019-09-09 12:37:35 +0700

    bintang

--- proseslatinputpros2.php
+++ proseslatinputpros2.php
@@ -1,10 +1,10 @@
 <?php
 
 if (isset($_POST['lanjut'])) {
-    $name = $_POST['nama'];
+    $name   = $_POST['nama'];
     $mapel1 = $_POST['mapel1'];
     $mapel2 = $_POST['mapel2'];
-    $rata = ($mapel1+$mapel2)/2;
+    $rata   = ($mapel1+$mapel2)/2;
     if ($rata > 75) {
         echo "Nama      : <b>$name</b><br>";
         echo "Mapel 1   : <b>$mapel1</b><br>";
